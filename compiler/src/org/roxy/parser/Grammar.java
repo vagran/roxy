97babfae3d7b528dd46b192e65334dc16cbc90c2
Artyom Lebedev <artyom.lebedev@gmail.com>
2016-01-03 13:58:53 +0200

    Implementing parser.

--- compiler/src/org/roxy/parser/Grammar.java
+++ compiler/src/org/roxy/parser/Grammar.java
@@ -1,5 +1,9 @@
 package org.roxy.parser;
 
+import java.util.ArrayList;
+import java.util.Map;
+import java.util.TreeMap;
+
 /** Language grammar description. */
 public class Grammar {
 
@@ -9,7 +13,14 @@ public class Node {
     public Node
     Name(String name)
     {
-        //XXX
+        if (this.name != null) {
+            throw new IllegalStateException("Node name already defined: " + this.name);
+        }
+        this.name = name;
+        if (nodesIndex.containsKey(name)) {
+            throw new IllegalArgumentException("Duplicated node name: " + name);
+        }
+        nodesIndex.put(name, this);
         return this;
     }
 
@@ -25,10 +36,21 @@ public class Node {
         return Quantity(num, num);
     }
 
+    /** Limit number of times to match the node. The matching is lazy in scope of parent node.
+     * Default quantification is (1;1).
+     *
+     * @param numMin Minimal number of matches.
+     * @param numMax Maximal number of matches. -1 for unlimited.
+     */
     public Node
     Quantity(int numMin, int numMax)
     {
-        //XXX
+        if (quantityValid) {
+            throw new IllegalStateException("Node quantity already defined");
+        }
+        quantityValid = true;
+        this.numMin = numMin;
+        this.numMax = numMax;
         return this;
     }
 
@@ -49,14 +71,51 @@ public class Node {
     {
         return Quantity(0, -1);
     }
+
+    protected String name;
+    protected int numMin, numMax;
+    protected boolean quantityValid = false;
+    /** Node is cloned into compiled tree. */
+    protected boolean isCloned = false;
+
+    protected Node
+    CopyTo(Map<String, Node> localIndex, Node node)
+    {
+        assert !isCloned;
+        node.name = name;
+        node.quantityValid = quantityValid;
+        node.numMin = numMin;
+        node.numMax = numMax;
+        node.isCloned = true;
+        if (name != null) {
+            assert !localIndex.containsKey(name);
+            localIndex.put(name, node);
+        }
+        return node;
+    }
+
+    protected Node
+    Clone(Map<String, Node> localIndex)
+    {
+        if (isCloned) {
+            return this;
+        }
+        return CopyTo(localIndex, new Node());
+    }
 }
 
 public class NodeBuilder {
 
+    private
+    NodeBuilder(String name)
+    {
+        this.name = name;
+    }
+
     public NodeBuilder
     Def(Node node)
     {
-        //XXX
+        node.Name(name);
         return this;
     }
 
@@ -71,83 +130,217 @@ public class NodeBuilder {
     {
         return Def(Grammar.this.Sequence(nodes));
     }
+
+    private final String name;
 }
 
+/** Transformed into sequence with one node. */
 public class NodeRef extends Node {
 
+    private
+    NodeRef(String refName)
+    {
+        this.refName = refName;
+    }
+
+    private Node
+    Resolve(Map<String, Node> localIndex)
+    {
+        Node node;
+        if ((node = localIndex.get(refName)) != null) {
+            return node;
+        }
+        if ((node = nodesIndex.get(refName)) != null) {
+            return node;
+        }
+        throw new IllegalStateException("Unresolved node reference: " + refName);
+    }
+
+    @Override protected Node
+    Clone(Map<String, Node> localIndex)
+    {
+        if (isCloned) {
+            return this;
+        }
+        return CopyTo(localIndex, new SequenceNode(Resolve(localIndex).Clone(localIndex)));
+    }
+
+    private final String refName;
 }
 
 public class CharNode extends Node {
 
+    /**
+     *
+     * @param matchAny Initialize with wildcard if true, empty set if false.
+     */
+    private
+    CharNode(boolean matchAny)
+    {
+        this.matchAny = matchAny;
+    }
+
+    public CharNode
+    Include(int c)
+    {
+        return Range(c, c, false);
+    }
+
+    public CharNode
+    Include(String chars)
+    {
+        chars.codePoints().forEach(c -> Range(c, c, false));
+        return this;
+    }
+
+    /** Add matched range. */
+    public CharNode
+    IncludeRange(int cMin, int cMax)
+    {
+        return Range(cMin, cMax, false);
+    }
+
+    public CharNode
+    Exclude(int c)
+    {
+        return Range(c, c, true);
+    }
+
     public CharNode
     Exclude(String chars)
     {
-        //XXX
+        chars.codePoints().forEach(c -> Range(c, c, true));
         return this;
     }
 
     public CharNode
     ExcludeRange(int cMin, int cMax)
     {
-        //XXX
+        return Range(cMin, cMax, true);
+    }
 
+    // /////////////////////////////////////////////////////////////////////////////////////////////
+
+    private class RangeEntry {
+        public int cMin, cMax;
+        public boolean exclude;
+
+        public
+        RangeEntry(int cMin, int cMax, boolean exclude)
+        {
+            this.cMin = cMin;
+            this.cMax = cMax;
+            this.exclude = exclude;
+        }
+    }
+
+    private final boolean matchAny;
+    private final ArrayList<RangeEntry> ranges = new ArrayList<>();
+
+    private CharNode
+    Range(int cMin, int cMax, boolean exclude)
+    {
+        ranges.add(new RangeEntry(cMin, cMax, exclude));
+        return this;
+    }
+
+    @Override protected Node
+    Clone(Map<String, Node> localIndex)
+    {
+        /* Character node is immutable so this instance can be returned. */
         return this;
     }
 }
 
-public class NodeGroup extends Node {
+public class SequenceNode extends Node {
 
+    private
+    SequenceNode(Node... nodes)
+    {
+        this.nodes = nodes;
+    }
+
+    private final Node[] nodes;
+
+    @Override protected Node
+    Clone(Map<String, Node> localIndex)
+    {
+        if (isCloned) {
+            return this;
+        }
+        Node[] newNodes = new Node[nodes.length];
+        for (int i = 0; i < nodes.length; i++) {
+            newNodes[i] = nodes[i].Clone(localIndex);
+        }
+        return CopyTo(localIndex, new SequenceNode(newNodes));
+    }
+}
+
+public class VariantsNode extends Node {
+
+    private
+    VariantsNode(Node... nodes)
+    {
+        this.nodes = nodes;
+    }
+
+    private final Node[] nodes;
+
+    @Override protected Node
+    Clone(Map<String, Node> localIndex)
+    {
+        if (isCloned) {
+            return this;
+        }
+        Node[] newNodes = new Node[nodes.length];
+        for (int i = 0; i < nodes.length; i++) {
+            newNodes[i] = nodes[i].Clone(localIndex);
+        }
+        return CopyTo(localIndex, new VariantsNode(newNodes));
+    }
 }
 
 public NodeBuilder
 Node(String name)
 {
-    //XXX
-    return null;
+    return new NodeBuilder(name);
 }
 
+/** Refer existing grammar node by name. */
 public NodeRef
 NodeRef(String name)
 {
-    //XXX
-
-    return null;
+    return new NodeRef(name);
 }
 
 public CharNode
 Char(int c)
 {
-    //XXX
-    return null;
+    return new CharNode(false).Include(c);
 }
 
 public CharNode
 CharRange(int cMin, int cMax)
 {
-    //XXX
-    return null;
+    return new CharNode(false).IncludeRange(cMin, cMax);
 }
 
 /** Matches any character. Exclusions cah be added in the returned node. */
 public CharNode
 AnyChar()
 {
-    //XXX
-    return null;
+    return new CharNode(true);
 }
 
 /** Matches any character from the provided characters set. */
 public CharNode
 AnyChar(String chars)
 {
-    //XXX
-    return null;
+    return new CharNode(false).Include(chars);
 }
 
-/** Characters sequence.
- * @return Last character node.
- */
-public CharNode
+/** Characters sequence. */
+public SequenceNode
 String(String string)
 {
     return String(string, true);
@@ -155,34 +348,49 @@ String(String string)
 
 /** Characters sequence.
  * @param caseSensitive Case sensitive match if true, case insensitive match otherwise.
- * @return Last character node.
  */
-public CharNode
+public SequenceNode
 String(String string, boolean caseSensitive)
 {
-    //XXX
-    return null;
+    ArrayList<Node> nodes = new ArrayList<>();
+    string.codePoints().forEach(c -> {
+        if (caseSensitive) {
+            nodes.add(Char(c));
+        } else {
+            nodes.add(Char(Character.toLowerCase(c)).Include(Character.toUpperCase(c)));
+        }
+    });
+    return Sequence(nodes.toArray(new Node[nodes.size()]));
 }
 
-public NodeGroup
+public SequenceNode
 Sequence(Node... nodes)
 {
-    //XXX
-    return null;
+    return new SequenceNode(nodes);
 }
 
-public NodeGroup
+public VariantsNode
 Any(Node... nodes)
 {
-    //XXX
-    return null;
+    return new VariantsNode(nodes);
 }
 
-//XXX
-public void
-Compile()
+/** Compile grammar into nodes tree.
+ *
+ * @param rootNodeName Name of the grammar root node.
+ * @return Root node of the compiled tree.
+ */
+public Node
+Compile(String rootNodeName)
 {
-    //XXX
+    TreeMap<String, Node> localIndex = new TreeMap<>();
+    Node root = nodesIndex.get(rootNodeName);
+    if (root == null) {
+        throw new IllegalArgumentException("Specified root node not found: " + rootNodeName);
+    }
+    return root.Clone(localIndex);
 }
+
+private final TreeMap<String, Node> nodesIndex = new TreeMap<>();
 
 }
--- compiler/test/org/roxy/parser/BasicTest.java
+++ compiler/test/org/roxy/parser/BasicTest.java
@@ -7,7 +7,23 @@ public class BasicTest {
 Grammar grammar = new Grammar() {{
 
     Node("decimal-digit").Def(CharRange('0', '9'));
+    Node("alphabetic").Def(CharRange('a', 'z').IncludeRange('A', 'Z'));
 
+    Node("multiline-comment").Sequence(String("/*"), AnyChar().NoneOrMany(), String("*/"));
+
+    Node("string-literal").Sequence(
+        Char('"'),
+        Any(AnyChar().Exclude("\"\\"),
+            Sequence(Char('\\'), AnyChar())),
+        Char('"'));
+
+    Node("identifier-first-char").Any(NodeRef("alphabetic"), Char('_'));
+    Node("identifier-char").Any(NodeRef("identifier-first-char"), NodeRef("decimal-digit"));
+    Node("identifier").Sequence(
+        NodeRef("identifier-first-char"),
+        NodeRef("identifier-char").NoneOrMany());
+
+    Compile("identifier");
 }};
 
 @Test public void
